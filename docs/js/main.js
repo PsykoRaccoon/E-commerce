27f8673156f214f7371c43b6f13543015448d576
Victor Sanchez <vickoss9004@gmail.com>
2024-11-19 13:37:37 -0600

    Arreglando las tonterias de luis

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,13 +1,1 @@
-import { app } from "./modules/config-app.js";
-//import { firebaseCRUD } from "./modules/crud.js";
-import { authEmail } from "./modules/auth-email.js";
-import { authGitHub } from "./modules/auth-github.js";
-import { authGoogle } from "./modules/auth-google.js";
-import { firebaseStorage } from "./modules/storage.js";
-
-console.log(app);
-//firebaseCRUD(app);
-authEmail(app);
-authGitHub(app);
-authGoogle(app);
-firebaseStorage(app);
+console.log("Hola Mundo desde 11ty 🦡🎈");
