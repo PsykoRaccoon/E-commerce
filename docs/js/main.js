5b6e67bb4e8b90b849fe64a88735e62424a88981
LuisLYiztino <luiselp.080972@gmail.com>
2024-11-19 10:42:04 -0600

    Agregando dependencias de Firebase
    Agregué lo necesario para que la API de firebase pueda funcionar

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo desde 11ty 🦡🎈");
+import { app } from "./modules/config-app.js";
+//import { firebaseCRUD } from "./modules/crud.js";
+import { authEmail } from "./modules/auth-email.js";
+import { authGitHub } from "./modules/auth-github.js";
+import { authGoogle } from "./modules/auth-google.js";
+import { firebaseStorage } from "./modules/storage.js";
+
+console.log(app);
+//firebaseCRUD(app);
+authEmail(app);
+authGitHub(app);
+authGoogle(app);
+firebaseStorage(app);
